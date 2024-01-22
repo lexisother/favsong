0215f0d60115ceecfd733a7f3b5696af449f395d
Alyxia Sother <alyxia@riseup.net>
2024-01-22 12:27:57 +0100

    [*] Pretty much finish up

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 import template from './template.html';
 import SimpleFMClient from '@solely/simple-fm';
+import { TrackType } from '@solely/simple-fm/dist/typings/index';
+
+type Song = TrackType & {
+	mbid: string;
+	listeners: number;
+};
 
 interface OpReq {
 	type: string;
@@ -13,59 +19,17 @@ const SFMClient = new SimpleFMClient('64346b97f7b9f6b97f29b37e3e1c521c');
 
 export default {
 	async fetch(request: Request, env: Env): Promise<Response> {
-		const defaultData = { song: {} };
-
-		// const setCache = (key, data) => env.EXAMPLE_TODOS.put(key, data);
-		// const getCache = (key) => env.EXAMPLE_TODOS.get(key);
-
-		// async function getTodos(request) {
-		// 	const ip = request.headers.get('CF-Connecting-IP');
-		// 	const cacheKey = `data-${ip}`;
-		// 	let data;
-		// 	const cache = await getCache(cacheKey);
-		// 	if (!cache) {
-		// 		await setCache(cacheKey, JSON.stringify(defaultData));
-		// 		data = defaultData;
-		// 	} else {
-		// 		data = JSON.parse(cache);
-		// 	}
-		//
-		// 	const body = template.replace(
-		// 		'$TODOS',
-		// 		JSON.stringify(
-		// 			data.todos?.map((todo) => ({
-		// 				id: escapeHtml(todo.id),
-		// 				name: escapeHtml(todo.name),
-		// 				completed: !!todo.completed,
-		// 			})) ?? []
-		// 		)
-		// 	);
-		// 	return new Response(body, {
-		// 		headers: { 'Content-Type': 'text/html' },
-		// 	});
-		// }
-
-		// async function updateTodos(request) {
-		// 	const body = await request.text();
-		// 	const ip = request.headers.get('CF-Connecting-IP');
-		// 	const cacheKey = `data-${ip}`;
-		// 	try {
-		// 		JSON.parse(body);
-		// 		await setCache(cacheKey, body);
-		// 		return new Response(body, { status: 200 });
-		// 	} catch (err) {
-		// 		return new Response(err, { status: 500 });
-		// 	}
-		// }
-		//
-		// if (request.method === 'PUT') {
-		// 	return updateTodos(request);
-		// }
-		// if (request.headers) return getTodos(request);
+		const setSong = (data: Song) => env.FAVSONG_KV.put('song', JSON.stringify(data));
+		const getSong = () => env.FAVSONG_KV.get('song');
 
 		const ops: Record<string, Function> = {
+			getCurrentSong: async (request: Request, body: OpReq) => {
+				let data = await getSong();
+				return new Response(data, {
+					headers: { 'Content-Type': 'application/json' },
+				});
+			},
 			search: async (request: Request, body: OpReq) => {
-				console.log('woa');
 				const query = body.query;
 				const data = await SFMClient.track.search({
 					track: query,
@@ -74,10 +38,20 @@ export default {
 					headers: { 'Content-Type': 'application/json' },
 				});
 			},
+			save: async (request: Request, body: OpReq) => {
+				const data = {};
+				await setSong(body.song);
+				return new Response(JSON.stringify(data), {
+					headers: { 'Content-Type': 'application/json' },
+				});
+			},
 		};
 
-		function showUI(request: Request): Response {
-			const body = template.replace('$FAVSONG', 'undefined');
+		async function showUI(request: Request): Promise<Response> {
+			let body = template;
+			const song = await getSong();
+			if (song) body = template.replace('$FAVSONG', `JSON.parse(${song})`);
+			else body = template.replace('$FAVSONG', undefined);
 
 			return new Response(body, {
 				headers: { 'Content-Type': 'text/html' },
